ef5931bd9d32fe0cb5e4e70382388a852049b359
tkachukione <tkachukione@gmail.com>
2021-07-27 18:15:58 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.5c9243f8c0fc4c9cc04a768006dfd735.js"
 );
 
 self.addEventListener('message', (event) => {
